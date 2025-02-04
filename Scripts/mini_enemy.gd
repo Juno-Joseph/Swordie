7fde8f0fec8fb2999892ad7b0db33cd26a98cb45
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-02-04 10:01:44 -0500

    enemy stuff

--- Scripts/mini_enemy.gd
+++ Scripts/mini_enemy.gd
@@ -2,8 +2,23 @@ extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
 
+var health : int = 30
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	animation_player.play("idle")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	move_and_slide()
 
 func take_damage(amount: int) -> void:
 	animation_player.play("hit")
+	var total : int = 0
+	total = amount + total
+	print("Total ", total)
 	print("Damage: ", amount)
+	
